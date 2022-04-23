07050ada29f6d419ed56c1f780eeb7505c642506
Acosta Demian Aaron <acostademiann14@gmail.com>
2022-04-22 21:46:38 -0300

    Update testViaje.php

--- TPOs/EmpresaDeTransporte2/testViaje.php
+++ TPOs/EmpresaDeTransporte2/testViaje.php
@@ -1,4 +1,13 @@
 <?php
+/* Modificar la clase Viaje para que ahora los pasajeros sean un objeto que tenga los atributos nombre, apellido, numero de documento y teléfono. 
+El viaje ahora contiene una referencia a una colección de objetos de la clase Pasajero. 
+También se desea guardar la información de la persona responsable de realizar el viaje, para ello cree una clase ResponsableV que registre el número de empleado,
+número de licencia, nombre y apellido. La clase Viaje debe hacer referencia al responsable de realizar el viaje.
+
+Volver a implementar las operaciones que permiten modificar el nombre, apellido y teléfono de un pasajero. 
+Luego implementar la operación que agrega los pasajeros al viaje, solicitando por consola la información de los mismos. 
+Se debe verificar que el pasajero no este cargado mas de una vez en el viaje. 
+De la misma forma cargue la información del responsable del viaje. */
 include "Pasajero.php";
 include "Viaje.php";
 include "ResponsableV.php";
