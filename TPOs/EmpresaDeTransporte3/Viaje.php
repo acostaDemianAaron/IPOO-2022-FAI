8de3cc33ddc68be886148a76c25b23a530f2d66b
Acosta Demian Aaron <acostademiann14@gmail.com>
2022-05-17 20:30:40 -0300

    Update Viaje.php

--- TPOs/EmpresaDeTransporte3/Viaje.php
+++ TPOs/EmpresaDeTransporte3/Viaje.php
@@ -194,7 +194,7 @@ class Viaje
     // Funciones magicas
     public function __toString()
     {
-        return "\nResponsable\n" . $this->getResposable() .
+        return "\nResponsable\n" . $this->getResponsable() .
             "\nDestino: " . $this->getDestino() .
             "\nCodigo: " . $this->getCodigo() .
             "\nCantidad maxima de pasajeros: " . $this->getCantMaxPasajeros() .
